d01b46c251fc6d26a3686494798a8803f58fea23
Inacio Alves <inacioc.alves@gmail.com>
2017-01-22 15:43:27 -0300

    Put errorbar and labels in the graphics

--- graphic_generator.py
+++ graphic_generator.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python3
 
 import numpy as np
-#import matplotlib.pyplot as plt
 
 def graphic(features, plt, title, sequence, colors):
 
     l = len(features[0])
     x = np.linspace(0,l*5,l,endpoint=True)
 
+    #fig, ax = plt.subplot(3, 4, sequence)
     plt.subplot(3, 4, sequence)
-    plt.title(title)
+    plt.xlabel('time(sec)')
+    plt.ylabel(title)
+    
+    l2 = ['src','dst']
+    l3 = ['min', 'mean', 'max']
+    lf = len(features)
 
-    # print(features)
-    # print(colors)
+    if lf==3:
+        l0=l3
+    elif lf==2:
+        l0=l2
+    else:
+        l0=['data']
 
-    for i in range(len(features)):        
-        if np.max(features[i])>10000:
-        #    #ax.ticklabel_format(style='sci',axis='y')
-            plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-        if type(colors[i])==str:
-            plt.plot(x, features[i], color=colors[i])
-        else:
-            plt.errorbar(x,features[i],fmt='--o', color=colors[i][1])
-            #plt.plot(x, features[i], fmt=colors[i][0], color=colors[i][1])
+    for i in range(lf):
+        
+        #if np.max(features[i])>1000:
+        plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        plt.plot(x, features[i], color=colors[i], label=l0[i])
 
+        if lf == 3 and i==1:
+            stdv = np.std(features[i])*np.ones(len(features[i]))            
+            plt.errorbar(x,features[i], yerr=stdv, fmt='ro')
+
+        plt.legend(loc='upper center', frameon=False, ncol=lf)
+        #plt.legend(loc='upper left', frameon=False)
+    
 
 def plot(features, plt):
     """
@@ -54,91 +66,23 @@ def plot(features, plt):
 
     c1 = ['blue']
     c2 = ['blue', 'green']
+    c3 = ['blue', 'green', 'black']
     c4 = ['blue', 'green', ['--o','red'],'black']
 
-    #print(dir(fig))
-
-    # plt.subplot(3, 4, 1)
-    # plt.title('H(ip_src)')
-    # plt.plot(x, f[0], color="blue")
-    # plt.plot(x, f[1], color="green")
     graphic([f[0],f[1]], plt, 'H(ip)', 1, c2)
-    
-
-    # plt.subplot(3, 4, 2)
-    # plt.title('H(port)')
-    # plt.plot(x, f[3], color="blue")
-    # plt.plot(x, f[4], color="green")
     graphic([f[3],f[4]], plt, 'H(port)', 2, c2)
+    graphic([f[5],f[6], f[8]], plt, 'packets', 3, c3)
+    graphic([f[9],f[10], f[12]], plt, 'bytes', 4, c3)
 
-    # plt.subplot(3, 4, 3)
-    # plt.title('packets')
-    # plt.plot(x, f[5], color="green")
-    # plt.plot(x, f[6], color="blue")
-    # plt.errorbar(x,f[7],fmt='--o', color="red")
-    # plt.plot(x, f[8], color="black")
-    graphic([f[5],f[6], f[7], f[8]], plt, 'packets', 3, c4)
-    
+    graphic([f[14],f[15], f[16]], plt, 'duration', 5, c3)
+    graphic([f[17],f[18], f[20]], plt, 'bytes/sec', 6, c3)
+    graphic([f[21],f[22], f[24]], plt, 'packets/sec', 7, c3)
+    graphic([f[25],f[26], f[28]], plt, 'bytes/packets', 8, c3)
 
-    # plt.subplot(3, 4, 4)
-    # plt.title('bytes')
-    # plt.plot(x, f[9], color="green")
-    # plt.plot(x, f[10], color="blue")
-    # plt.errorbar(x,f[11],fmt='--o', color="red")
-    # plt.plot(x, f[12], color="black")
-    graphic([f[9],f[10], f[11], f[12]], plt, 'bytes', 4, c4)
-
-    # plt.subplot(3, 4, 5)
-    # plt.title('duration')
-    # plt.plot(x, f[13], color="green")
-    # plt.plot(x, f[14], color="blue")
-    # plt.errorbar(x,f[15],fmt='--o', color="red")
-    # plt.plot(x, f[16], color="black")
-    graphic([f[14],f[15], f[15], f[16]], plt, 'duration', 5, c4)
-
-    # plt.subplot(3, 4, 6)
-    # plt.title('bytes/sec')
-    # plt.plot(x, f[17], color="green")
-    # plt.plot(x, f[18], color="blue")
-    # plt.errorbar(x,f[19],fmt='--o', color="red")
-    # plt.plot(x, f[20], color="black")
-    graphic([f[17],f[18], f[19], f[20]], plt, 'bytes/sec', 6, c4)
-
-    # plt.subplot(3, 4, 7)
-    # plt.title('packets/sec')
-    # plt.plot(x, f[21], color="green")
-    # plt.plot(x, f[22], color="blue")
-    # plt.errorbar(x,f[23],fmt='--o', color="red")
-    # plt.plot(x, f[24], color="black")
-    graphic([f[21],f[22], f[23], f[24]], plt, 'packets/sec', 7, c4)
-
-    # plt.subplot(3, 4, 8)
-    # plt.title('bytes/packet')
-    # plt.plot(x, f[25], color="green")
-    # plt.plot(x, f[26], color="blue")
-    # plt.errorbar(x,f[27],fmt='--o', color="red")
-    # plt.plot(x, f[28], color="black")
-    graphic([f[25],f[26], f[27], f[28]], plt, 'bytes/packets', 8, c4)
-
-    # plt.subplot(3, 4, 9)
-    # plt.title('bytes/flow')
-    # plt.plot(x, f[29], color="green")
     graphic([f[29]], plt, 'bytes/flow', 9, c1)
-
-    # plt.subplot(3, 4, 10)
-    # plt.title('packets/flow')
-    # plt.plot(x, f[30], color="green")
     graphic([f[30]], plt, 'packets/flow', 10, c1)
-
-    # plt.subplot(3, 4, 11)
-    # plt.title('flow duration')
-    # plt.plot(x, f[31], color="green")
     graphic([f[31]], plt, 'flow duration', 11, c1)
-
-    # plt.subplot(3, 4, 12)
-    # plt.title('flows/sec')
-    # plt.plot(x, f[32], color="green")
-    graphic([f[32]], plt, 'flows/sec', 12, c1)
+    graphic([f[33]], plt, 'flows/sec', 12, c1)
     
 
 
